d0420598d235e7de45c7b3007e9e723b0df877b7
Mikko Heikkinen <hawk@biomi.org>
2026-02-17 00:03:27 +0200

    Add player, improve styles

--- traxbase/routes.py
+++ traxbase/routes.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, redirect, render_template, url_for
+from flask import Blueprint, redirect, render_template, send_from_directory, url_for
 
 from traxbase.db import get_all_tracks, rebuild_tracks
-from traxbase.scanner import scan_music_dir
+from traxbase.scanner import MUSIC_DIR, scan_music_dir
 
 main = Blueprint("main", __name__)
 
@@ -17,3 +17,8 @@ def update():
     tracks = scan_music_dir()
     rebuild_tracks(tracks)
     return redirect(url_for("main.index"))
+
+
+@main.route("/audio/<path:filepath>")
+def serve_audio(filepath):
+    return send_from_directory(MUSIC_DIR, filepath)
